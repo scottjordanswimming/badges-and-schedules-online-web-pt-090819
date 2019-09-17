91e738c693029c12d83acbd72d7207ebc7b28cca
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-17 19:45:59 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,43 @@
-# Write your code here.
+
+
+
+  
+  def badge_maker(name)
+    return "Hello, my name is #{name}."
+  end
+  
+  def batch_badge_creator(array)
+
+  names = []
+  
+  array.each { |name| names << "Hello, my name is #{name}."}
+  
+  names
+  
+end
+
+def assign_rooms(array)
+  
+  namerooms = []
+  
+ 
+  
+
+  array.each_with_index { |name, i| namerooms << "Hello, #{name}! You'll be assigned to room #{i + 1}!"}
+  
+
+  
+  namerooms
+  
+  
+  
+end
+
+def printer(attendees)
+  printer_list = batch_badge_creator(attendees).concat assign_rooms(attendees)
+  printer_list.each do |line|
+    puts line
+  end
+end
+  
+   
